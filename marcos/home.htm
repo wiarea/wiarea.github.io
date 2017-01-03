8225ca19654ef8791c1c6d3ea4ac3a19dd3ffdc7
wiarea <mariaagmen@gmail.com>
2017-01-03 18:06:43 +0000

    Añadiendo el resto de ficheros

---
+++ marcos/home.htm
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<style type="text/css">
+<!--
+.t {
+	font-family: "Comic Sans MS", Verdana, "Lucida Console", sans-serif;
+}
+.t strong {
+	font-size: 16px;
+}
+.t strong {
+	color: #CF6;
+}
+.t strong {
+	font-size: 24px;
+}
+.t strong {
+	font-size: 18px;
+}
+body {
+	background-color: #6CC;
+}
+.t strong {
+	color: #000;
+}
+-->
+</style></head>
+
+<body>
+<div align="center">
+  <p><img src="../imagenes/final/anagrama2.gif" width="188" height="129" alt="anagrama" /></p>
+  <p class="t"><strong>Dirección: C/ Juan Bautista Nº19</strong></p>
+  <p class="t"><strong>Tlf:822-60-45-32 </strong></p>
+</div>
+</body>
+</html>
---
+++ marcos/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #6CC;
+}
+-->
+</style></head>
+
+<body>
+<p><a href="central.html" target="mainFrame"><img src="../imagenes/final/img_barranavega/img1_naranja.gif" width="150" height="40" alt="inicio" /></a>
+</p>
+<p><a href="../catalogo/catalogo.html" target="mainFrame"><img src="../imagenes/final/img_barranavega/img2_naranja.gif" width="150" height="40" alt="catalogo" /></a></p>
+<p><a href="../registro.html" target="mainFrame"><img src="../imagenes/final/img_barranavega/img3_naranja.gif" width="150" height="40" alt="registro" /></a></p>
+<p><a href="../contacto.html" target="mainFrame"><img src="../imagenes/final/img_barranavega/img4_naranja.gif" width="150" height="40" alt="contacto" /></a></p>
+</body>
+</html>
